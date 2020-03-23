0b3e85b0e4b1fba3f60edff1de2e8eb1262cd645
liukunkun <1172663762@qq.com>
2020-03-23 21:44:28 +0800

    构建项目、配置全局样式、配置路由、使用elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">这里是登录页<el-button>默认按钮</el-button></div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>//scoped使css只在该文件中有效，不会污染其他文件的css
+#login {
+    height: 100vh;//vh基于窗口大小自适应，50vh始终占窗口大小的一半，随窗口大小的变动而一起变动
+    background-color: #344a5f;
+}
+</style>
